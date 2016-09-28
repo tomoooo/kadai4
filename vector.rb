38edcaa9af86e582b009ac21ce2763ed323f2323
j1233 <j1233>
2016-09-28 13:42:27 +0900

    addメソッドを追加

--- vector.rb
+++ vector.rb
@@ -5,14 +5,32 @@ class Vector
   end
 
   def to_s
-    puts "\"(#{@x},#{@y})\""
+   "\"(#{@x},#{@y})\""
   end
     
   def length
     ans = Math::sqrt(@x*@x + @y*@y)
     puts ans
   end
+
+  def x
+    @x
+  end
+
+  def y
+    @y
+  end
+
+  def add(v)
+    Vector.new(@x+v.x , @y+v.y)
+  end
 end
+
+v1 = Vector.new(1,2)
+v2 = Vector.new(3,4)
+v3 = v1.add(v2)
+puts v3.to_s
+
 
 
 
